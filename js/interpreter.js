abef1d2d9aa5bd173db789d887551951147c676e
jinkool <jinkyulim96@gmail.com>
2019-11-13 23:14:02 +1300

    updated interpreter

--- js/interpreter.js
+++ js/interpreter.js
@@ -1,108 +1,104 @@
 
-window.onload = function(){ 
-    // your code 
-document.getElementById('interpret').onclick = function() {
-   interpret(document.getElementById('editor').value)
- };
+$(document).ready(function() {
+  $("#interpret").click(function(){
+    interpret(document.getElementById('editor').value)
+  });
 
-function interpret(s){
+  async function interpret(s){
+    $("#output").val("");
+    var mem = [0];
+    var mem_pos = 0;
+    var reg = 0;
+    var brac_open_pos = [];
 
-var mem = [0];
-var mem_pos = 0;
-var reg = 0;
-var brac_open_pos = [];
-
-for (var i = 0; i <s .length; i++) {
-    if (s.charAt(i) == 'w') {
-      if (s.charAt(++i) == 'o') {
-        if (s.charAt(++i) == 'w') {
+    var i = 0;
+    while (i<s.length-2){
+      await sleep(10);
+      i = i + 3;
+      switch(s.substring(i - 3,i)){
+        case "wow":
           mem[mem_pos]--;
-        }
-        else if (s.charAt(i) == 'W') {
-          if (mem_pos == mem.length) {
+          break;
+        case "woW":
+          if (mem_pos == mem.length - 1) {
             mem.push(0);
           }
           mem_pos++;
-        }
-        else if (s.charAt(i) == 'o') {
-          if (s.charAt(++i) == 'w') {
-            reg = mem[mem_pos];
+          if ($('#memory-container').children().last().attr('id')!=("line" + Math.floor(mem_pos/8))){
+            $("#memory-container").append("<div id=\"line" + Math.floor(mem_pos/8) + "\">0 0 0 0 0 0 0 0</div>");
           }
-          else {
-            i--;
-          }
-        }
-        else {
-          i--;
-        }
-      }
-      else if (s.charAt(i) == 'O') {
-        if (s.charAt(++i) == 'w') {
-          //input from std?
-        }
-        else if (s.charAt(i) == 'W') {
+          break;
+        case "wOw":
+          break;
+        case "wOW":
           if (mem[mem_pos]!=0){
             i = brac_open_pos[brac_open_pos.length-1];
           }
-        }
-        else {
-          i--;
-        }
-      }
-      else if (s.charAt(i) == '0'){
-        if (s.charAt(++i) == 'w') {
-          mem[mem_pos] = 0;
-        }
-        else {
-          i--;
-        }
-      }
-      else {
-        i--;
-      }
-    }
-    else if (s.charAt(i) == 'W') {
-      if (s.charAt(++i) == 'o') {
-        if (s.charAt(++i) == 'w') {
+          break;
+        case "Wow":
           mem_pos--;
-        }
-        else if (s.charAt(i) == 'W') {
+          break;
+        case "WoW":
           document.getElementById("output").value+=String.fromCharCode(mem[mem_pos]);
-        }
-        else {
-          i--;
-        }
-      }
-      else if (s.charAt(i) == 'O') {
-        if (s.charAt(++i) == 'w') {
+          break;
+        case "WOw":
           brac_open_pos.push(i + 1);
           if (mem[mem_pos] == 0) {
             i = i + 1 + s.substring(i+1).indexof("wOW");
             brac_open_pos.pop();
           }
-        }
-        else if (s.charAt(i) == 'W') {
+          break;
+        case "WOW":
           mem[mem_pos]++;
-        }
-        else if (s.charAt(i) == 'O') {
-          if (s.charAt(++i) == 'W') {
-            mem[mem_pos] += reg;
+          processLine(mem,mem_pos);
+          break;
+        case "w0w":
+          mem[mem_pos] = 0;
+          break;
+        case "woo":
+          if (s[i]=="w"){
+              reg = mem[mem_pos];
+              i++;
           }
-          else {
-            i--;
+          else{
+            i = i - 2;
           }
-        }
-        else {
-          i--;
+          break;
+        case "WOO":
+          if (s[i]=="W"){
+              mem[mem_pos] += reg;
+            i++;
+          }
+          else{
+            i = i - 2;
+          }
+          break;
+        default:
+          i = i - 2;
         }
       }
-      else {
-        i--;
-      }
-    }
+
+
   }
 
+  function processLine(mem,mem_pos){
 
+    var startPos = Math.floor(mem_pos/8) * 8;
+    var output = "";
 
+    for (var i=startPos; i<startPos+8; i++ ){
+      if (i > mem.length-1){
+        output += "0 ";
+      }
+      else{
+        output += mem[i] + " ";
+      }
+    }
+
+    $("#line" + Math.floor(mem_pos/8)).text(output);
+  }
+  function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
-};
+
+});
