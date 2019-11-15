79a8b517cae3ad109cb2299089e685648aa61a3a
Heath Logan Campbell <github@heathlogancampbell.com>
2019-11-15 22:47:51 +1300

    add constants

--- js/interpreter.js
+++ js/interpreter.js
@@ -1,66 +1,89 @@
 
 $(document).ready(function () {
-  const CMD_MEMORY_INC = "woW"
-  const CMD_MEMORY_DEC = "Wow"
+  const CMD_MEMORY_INC = "WOW"
+  const CMD_MEMORY_DEC = "wow"
+  const CMD_REG_INC = "woW"
+  const CMD_REG_DEC = "Wow"
+  const CMD_INPUT = "wOw"
+  const CMD_OUTPUT = "WoW"
+  const CMD_RESET = "w0w"
+  const CMD_LOOP_OPEN = "WOw"
+  const CMD_LOOP_CLOSE = "wOW"
 
-  $("#interpret").click(function () {
-    interpret(document.getElementById('editor').value)
+  const HTML_EDITOR_ID = "editor";
+  const HTML_OUTPUT_ID = "output";
+  const HTML_INPUT_ID = "stdin";
+  const HTML_RUN_BTN_ID = "interpret";
+  const HTML_DELAY_ID = "delay";
+  const HTML_MEMORY_ID = "memory-container";
+
+  $("#" + HTML_RUN_BTN_ID).click(function () {
+    interpret(document.getElementById(HTML_EDITOR_ID).value)
   });
 
   async function interpret(s) {
-    $("#output").val("");
+    $("#" + HTML_OUTPUT_ID).val("");
     var mem = [0];
     var mem_pos = 0;
     var reg = 0;
     var brac_open_pos = [];
-    var input = $("#stdin").val().split(" ");
+    var input = $("#" + HTML_INPUT_ID).val().split(" ");
     var current_input_pos = 0;
 
     var i = 0;
     while (i < s.length - 2) {
-      await sleep($("#delay").val() * 10);
+      await sleep($("#" + HTML_DELAY_ID).val() * 10);
       i = i + 3;
       switch (s.substring(i - 3, i)) {
-        case "wow":
+
+        case CMD_MEMORY_DEC:
           mem[mem_pos]--;
           processLine(mem, mem_pos);
           break;
-        case CMD_MEMORY_INC:
+
+        case CMD_REG_INC:
           if (mem_pos == mem.length - 1) {
             mem.push(0);
           }
           mem_pos++;
-          if ($('#memory-container').children().last().attr('id') != ("line" + Math.floor(mem_pos / 8))) {
-            $("#memory-container").append("<div id=\"line" + Math.floor(mem_pos / 8) + "\">0 0 0 0 0 0 0 0</div>");
+          if ($('#' + HTML_MEMORY_ID).children().last().attr('id') != ("line" + Math.floor(mem_pos / 8))) {
+            $("#" + HTML_MEMORY_ID).append("<div id=\"line" + Math.floor(mem_pos / 8) + "\">0 0 0 0 0 0 0 0</div>");
           }
           break;
-        case "wOw":
-          mem[mem_pos]=parseInt(input[current_input_pos++]);
-          processLine(mem,mem_pos);
+
+        case CMD_INPUT:
+          mem[mem_pos] = parseInt(input[current_input_pos++]);
+          processLine(mem, mem_pos);
           break;
-        case "wOW":
+
+        case CMD_LOOP_CLOSE:
           if (mem[mem_pos] != 0) {
             i = brac_open_pos[brac_open_pos.length - 1];
           }
           break;
-        case CMD_MEMORY_DEC:
+
+        case CMD_REG_DEC:
           mem_pos--;
           break;
-        case "WoW":
-          document.getElementById("output").value += String.fromCharCode(mem[mem_pos]);
+
+        case CMD_OUTPUT:
+          document.getElementById(HTML_OUTPUT_ID).value += String.fromCharCode(mem[mem_pos]);
           break;
-        case "WOw":
+
+        case CMD_LOOP_OPEN:
           brac_open_pos.push(i + 1);
           if (mem[mem_pos] == 0) {
-            i = i + 1 + s.substring(i + 1).indexof("wOW");
+            i = i + 1 + s.substring(i + 1).indexof(CMD_LOOP_CLOSE);
             brac_open_pos.pop();
           }
           break;
-        case "WOW":
+
+        case CMD_MEMORY_INC:
           mem[mem_pos]++;
           processLine(mem, mem_pos);
           break;
-        case "w0w":
+
+        case CMD_RESET:
           mem[mem_pos] = 0;
           break;
         case "woo":
