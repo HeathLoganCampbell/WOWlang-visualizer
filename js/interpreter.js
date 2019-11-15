5e7fb559df529f8d5f5a13dd0bd4927b1b05037b
Heath Logan Campbell <github@heathlogancampbell.com>
2019-11-15 22:37:18 +1300

    add constants

--- js/interpreter.js
+++ js/interpreter.js
@@ -1,10 +1,13 @@
 
-$(document).ready(function() {
-  $("#interpret").click(function(){
+$(document).ready(function () {
+  const CMD_MEMORY_INC = "woW"
+  const CMD_MEMORY_DEC = "Wow"
+
+  $("#interpret").click(function () {
     interpret(document.getElementById('editor').value)
   });
 
-  async function interpret(s){
+  async function interpret(s) {
     $("#output").val("");
     var mem = [0];
     var mem_pos = 0;
@@ -12,110 +15,110 @@ $(document).ready(function() {
     var brac_open_pos = [];
 
     var i = 0;
-    while (i<s.length-2){
-      await sleep($("#delay").val()*10);
+    while (i < s.length - 2) {
+      await sleep($("#delay").val() * 10);
       i = i + 3;
-      switch(s.substring(i - 3,i)){
+      switch (s.substring(i - 3, i)) {
         case "wow":
           mem[mem_pos]--;
-          processLine(mem,mem_pos);
+          processLine(mem, mem_pos);
           break;
-        case "woW":
+        case CMD_MEMORY_INC:
           if (mem_pos == mem.length - 1) {
             mem.push(0);
           }
           mem_pos++;
-          if ($('#memory-container').children().last().attr('id')!=("line" + Math.floor(mem_pos/8))){
-            $("#memory-container").append("<div id=\"line" + Math.floor(mem_pos/8) + "\">0 0 0 0 0 0 0 0</div>");
+          if ($('#memory-container').children().last().attr('id') != ("line" + Math.floor(mem_pos / 8))) {
+            $("#memory-container").append("<div id=\"line" + Math.floor(mem_pos / 8) + "\">0 0 0 0 0 0 0 0</div>");
           }
           break;
         case "wOw":
           break;
         case "wOW":
-          if (mem[mem_pos]!=0){
-            i = brac_open_pos[brac_open_pos.length-1];
+          if (mem[mem_pos] != 0) {
+            i = brac_open_pos[brac_open_pos.length - 1];
           }
           break;
-        case "Wow":
+        case CMD_MEMORY_DEC:
           mem_pos--;
           break;
         case "WoW":
-          document.getElementById("output").value+=String.fromCharCode(mem[mem_pos]);
+          document.getElementById("output").value += String.fromCharCode(mem[mem_pos]);
           break;
         case "WOw":
           brac_open_pos.push(i + 1);
           if (mem[mem_pos] == 0) {
-            i = i + 1 + s.substring(i+1).indexof("wOW");
+            i = i + 1 + s.substring(i + 1).indexof("wOW");
             brac_open_pos.pop();
           }
           break;
         case "WOW":
           mem[mem_pos]++;
-          processLine(mem,mem_pos);
+          processLine(mem, mem_pos);
           break;
         case "w0w":
           mem[mem_pos] = 0;
           break;
         case "woo":
-          if (s[i]=="w"){
-              reg = mem[mem_pos];
-              i++;
+          if (s[i] == "w") {
+            reg = mem[mem_pos];
+            i++;
           }
-          else{
+          else {
             i = i - 2;
           }
           break;
         case "WOO":
-          if (s[i]=="W"){
-              mem[mem_pos] += reg;
+          if (s[i] == "W") {
+            mem[mem_pos] += reg;
             i++;
           }
-          else{
+          else {
             i = i - 2;
           }
           break;
         default:
           i = i - 2;
-        }
       }
+    }
 
 
   }
 
-  function processLine(mem,mem_pos){
+  function processLine(mem, mem_pos) {
 
-    var startPos = Math.floor(mem_pos/8) * 8;
+    var startPos = Math.floor(mem_pos / 8) * 8;
     var output = "";
-    var display ="";
-    for (var i=startPos; i<startPos+8; i++ ){
-      if (i == mem_pos){
+    var display = "";
+    for (var i = startPos; i < startPos + 8; i++) {
+      if (i == mem_pos) {
         output += " <span class=\"current-memory\"> "
         display += "<span class=\"current-memory\">"
       }
-      if (i > mem.length-1){
+      if (i > mem.length - 1) {
         output += "00 ";
         display += ".";
       }
-      else{
-        output += mem[i].toString(16).toUpperCase().padStart(2,'0') + " ";
-        if ((mem[i]>=32)&&(mem[i]<127)){
+      else {
+        output += mem[i].toString(16).toUpperCase().padStart(2, '0') + " ";
+        if ((mem[i] >= 32) && (mem[i] < 127)) {
           display += String.fromCharCode(mem[i]);
         }
-        else{
+        else {
           display += ".";
         }
       }
-      if (i==mem_pos){
+      if (i == mem_pos) {
         output += " </span> "
         display += "</span>"
       }
     }
     output += " " + display;
 
-    $("#line" + Math.floor(mem_pos/8)).html(output);
+    $("#line" + Math.floor(mem_pos / 8)).html(output);
   }
   function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
 });
